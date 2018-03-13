290fe2c70f2f09317b627645c77c9c5ac045e878
Gar Cheuy <garcheuy@yahoo.com>
2018-03-13 13:42:04 -0700

    Updated commit

--- logic.js
+++ logic.js
@@ -24,6 +24,8 @@ d3.json(queryUrl, function(data) {
 
 function createFeatures(earthquakeData) {
 
+    // feature.properties.mag
+
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     // function handleFeature(feature, layer) {
@@ -41,8 +43,31 @@ function createFeatures(earthquakeData) {
 
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
-    console.log(earthquakes);
+    // console.log(earthquakeData);
 }
+
+// function markerSize(population) {
+//     return population / 25;
+// }
+
+// var getColors2 = d3.scaleLinear()
+//     .domain(d3.extent(cities, function(city){
+//         return city.population;
+//     }))
+//     .range(['steelblue', 'limegreen']);
+
+// for (var i = 0; i < cities.length; i++) {
+//     L.circle(cities[i].location, {
+//         fillOpacity: 0.75,
+//         fillColor: getColors2(cities[i].population),
+//         color: "none",
+//         weight: 3,
+//         className: "city",
+//         // Setting our circle's radius equal to the output of our markerSize function
+//         // This will make our marker's size proportionate to its population
+//         radius: markerSize(cities[i].population)
+//     }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+// }
 
 function createMap(earthquakes) {
 
